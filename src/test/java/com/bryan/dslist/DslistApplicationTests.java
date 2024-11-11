66c3bfc602d9079ef17dcdc45f6dbe4d0601f0f2
Bryan <bryan.gps18@gmail.com>
2024-11-11 10:11:29 -0300

    Project created

---
+++ src/test/java/com/bryan/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryan.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
